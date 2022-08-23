82ad92810f91c20da5ff9b47c281aaf1ad6e0361
kitoyjami <weurbanor@unitru.edu.pe>
2022-08-23 00:51:05 -0500

    added new test for bookcontroller

---
+++ api/test/database.js
@@ -0,0 +1,34 @@
+import  mongoose from 'mongoose'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+let mongoServer
+
+/**
+ * Connect
+ * close
+ * clear
+ */
+
+const connect = async() => {
+    mongoServer = await MongoMemoryServer.create()
+    await mongoose.connect(mongoServer.getUri())
+}
+
+const close = async () => {
+    await mongoose.connection.dropDatabase()
+    await mongoose.connection.close()
+    await mongoServer.stop()
+}
+
+const clear = async() => {
+
+    const collections = mongoose.connection.collections
+    for(const key in collections)
+    {
+        await collections[key].deleteMany()
+    }
+    
+}
+
+export {connect, clear, close}
+
